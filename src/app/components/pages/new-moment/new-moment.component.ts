fa2d1cb4bc60e8c4687644b233972edd5d09f02e
leandro <l34ndr0penha@gmail.com>
2023-08-12 16:43:48 -0300

    inserindo dados no sistema

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -7,5 +9,26 @@ import { Component } from '@angular/core';
 })
 export class NewMomentComponent {
   btnText = "Compartilhar!"
+
+  constructor(private momentService: MomentService) {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+    if (moment.image) {
+      formData.append("image", moment.image)
+    }
+
+    // todo
+
+   await this.momentService.createMoment(formData).subscribe()
+
+    //exibir mensagem
+
+    //redirect
+  }
 
 }
