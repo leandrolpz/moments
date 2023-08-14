12c72c3d95f0ccfa29fac92b6194197ffd525870
leandro <l34ndr0penha@gmail.com>
2023-08-14 11:57:45 -0300

    excluindo registro

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 import { Moment } from 'src/app/Moment';
 import { environment } from 'src/environments/environment.development';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
@@ -17,11 +18,22 @@ export class MomentComponent {
   faTimes = faTimes
   faEdit = faEdit
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute,
+    private messageService: MessagesService,
+    private router: Router) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'))
     this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data))
+  }
+
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe()
+
+    this.messageService.add("Momento Excluido Com Sucesso!")
+    this.router.navigate(['/'])
   }
 
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs'
+import { Observable } from 'rxjs'
 import { environment } from 'src/environments/environment.development';
 
 
@@ -12,7 +12,7 @@ import { Response } from '../Response';
 })
 export class MomentService {
   private baseApiUrl = environment.baseApiUrl;
-  private apiUrl =  `${this.baseApiUrl}api/moments`;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) { }
 
@@ -25,7 +25,12 @@ export class MomentService {
     return this.http.get<Response<Moment>>(url)
   }
 
-  createMoment(formData: FormData): Observable<FormData>  {
+  createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
+  removeMoment(id: number) {
+    const url = `${this.apiUrl}/${id}`
+    return this.http.delete(url)
   }
 }
