8ca3671f675b34b04610dbdef91977ef9cbf967a
leandro <l34ndr0penha@gmail.com>
2023-08-14 11:47:15 -0300

    css do momento

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment.development';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -10,6 +12,10 @@ import { Moment } from 'src/app/Moment';
 })
 export class MomentComponent {
   moment?: Moment
+  baseApiUrl = environment.baseApiUrl
+
+  faTimes = faTimes
+  faEdit = faEdit
 
   constructor(private momentService: MomentService, private route: ActivatedRoute) { }
 
