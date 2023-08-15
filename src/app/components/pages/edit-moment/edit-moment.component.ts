24f229ff0fce585acee844c38045cb1112472582
leandro <l34ndr0penha@gmail.com>
2023-08-15 10:29:23 -0300

    finalizando edit de registro & form de comments

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesComponent } from '../../messages/messages.component';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -12,7 +14,11 @@ export class EditMomentComponent {
   moment!: Moment
   btnText: string = 'Editar'
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router) { }
 
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'))
@@ -21,4 +27,24 @@ export class EditMomentComponent {
       this.moment = item.data
     })
   }
+
+  async editHandler(momentData: Moment) {
+    const id = this.moment.id
+    const formData = new FormData()
+
+    formData.append('title', momentData.title)
+    formData.append('description', momentData.description)
+
+    if (momentData.image) {
+      formData.append('image', momentData.image)
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe(() => {
+      this.messagesService.add(`Momento ${id} foi atualizado com sucesso`)
+
+      this.router.navigate(['/'])
+    })
+
+  }
+
 }
